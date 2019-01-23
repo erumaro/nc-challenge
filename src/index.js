48ac9fa507a396a6b82a147b3ba16d5e7979fd99
Tobias Årud <me@tobiasarud.se>
2019-01-23 17:55:58 +0100

    Day 1
    For Day 2:
    Ask about textFit or building own solution.
    Unit test functions, event listeners etc.
    Fix bugs and refactor Day 1 code. Add IEdge CSS.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 const inputText = document.getElementById('change-text');
 const inputRange = document.getElementById('change-width');
 const formOutput = document.getElementById('form-output');
+
+inputText.value = localStorage.getItem('currentDivText');
+inputRange.value = localStorage.getItem('currentDivWidth');
+formOutput.style.width = `${localStorage.getItem('currentDivWidth')}px`;
+formOutput.innerText = localStorage.getItem('currentDivText');
+
+textFit(formOutput);
+
+inputRange.addEventListener('change', setNewWidthOfDiv);
+inputText.addEventListener('input', writeTextInsideDiv);
+
+function setNewWidthOfDiv() {
+    formOutput.style.width = `${inputRange.value}px`;
+    localStorage.setItem('currentDivWidth', this.value);
+    textFit(formOutput);
+}
+
+function writeTextInsideDiv() {
+    formOutput.innerText = inputText.value;
+    localStorage.setItem('currentDivText', this.value);
+    textFit(formOutput);
+}
+
+// localStorage.setItem('key', 'value'); // Ex. 'Width', 200
+// localStorage.clear(); // Create a button to reset forms.
